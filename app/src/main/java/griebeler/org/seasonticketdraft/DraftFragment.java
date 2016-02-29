4a868fddce918834f0c935102abee0fae93e8c3f
Jason Griebeler <jason.griebeler@isobar.com>
2016-02-29 15:34:00 -0600

    custom adapter.

--- app/src/main/java/griebeler/org/seasonticketdraft/DraftFragment.java
+++ app/src/main/java/griebeler/org/seasonticketdraft/DraftFragment.java
@@ -9,7 +9,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.ListView;
 
@@ -32,7 +31,8 @@ public class DraftFragment extends Fragment {
         View draftView = inflater.inflate(R.layout.draft_fragment, container, false);
 
         List<String> draftOrder = new ArrayList<>();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.draft_order_view, R.id.draft_order_participant_textview, draftOrder);
+        DraftInstanceAdapter adapter = new DraftInstanceAdapter(getActivity(), R.layout.draft_instance_view, R.id.participant_textview, draftOrder);
+
         ListView listView = (ListView)draftView.findViewById(R.id.draft_order_listview);
         listView.setAdapter(adapter);
 
